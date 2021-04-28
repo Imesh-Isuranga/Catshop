6814e3a045dd2818a62a5bb0834d3941d2143b38
constandinos1999 <constandinos1999@hotmail.com>
2021-04-28 18:24:53 +0900

    // connect to mysql db

--- dbAccess/LinuxAccess.java
+++ dbAccess/LinuxAccess.java
@@ -9,11 +9,21 @@ class LinuxAccess extends DBAccess
 {
   public void loadDriver() throws Exception
   {
-    Class.forName("org.gjt.mm.mysql.Driver").newInstance();
+//    Class.forName("org.gjt.mm.mysql.Driver").newInstance();
+    Class.forName("com.mysql.jdbc.Driver").newInstance();
   }
 
   public String urlOfDatabase()
   {
-    return "jdbc:mysql://localhost/cshop?user=root";
+//    return "jdbc:mysql://localhost/cshop?user=root";
+    return "jdbc:mysql://localhost:3306/cshop";
+  }
+  public String username()
+  {
+    return "root";
+  }
+  public String password()
+  {
+    return "admin@123";
   }
 }
