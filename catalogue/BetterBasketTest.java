e47d98680ad36d4c62b060ecfca5218ff10d832e
constandinos1999 <constandinos1999@hotmail.com>
2021-05-08 14:07:13 +0700

    add BetterBasketTest.java

---
+++ catalogue/BetterBasketTest.java
@@ -0,0 +1,14 @@
+package catalogue;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BetterBasketTest {
+
+	@Test
+	public void testAddProduct() {
+		fail("Not yet implemented");
+	}
+
+}
